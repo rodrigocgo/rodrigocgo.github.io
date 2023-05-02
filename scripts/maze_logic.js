eb2b86f6ce30482a93234a7b6202ec1d9af29945
rodrigocgo <rodrigo.gon95@gmail.com>
2023-05-02 19:49:14 -0300

    Novas features

--- scripts/maze_logic.js
+++ scripts/maze_logic.js
@@ -1,4 +1,8 @@
-let bCriouMaze = false;
+let bCriouMaze   = false;
+let bTimeoutMAze = false;
+let bStartouGame = false;
+let bSaiuMaze    = false;
+let bClicou      = false;
 
 AFRAME.registerComponent("image-switcher", {
    init: function () {
@@ -18,37 +22,249 @@ AFRAME.registerComponent("image-switcher", {
    },
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-   const pilar = document.querySelector('#ground');
-   const player = document.querySelector('#player');
+function StartGame() {
+   escreveBoardLetraMenor("Clique na tela para começar");
+   
+   document.addEventListener('click', function(event) {
+      if (!bClicou) {
+         bClicou = true;
+         initGame();
+         contadorMaze();
+      }
+    });
+}
 
+
+function initGame(){
+   
+   var attr_text = 6;
+   
+   
+   setTimeout(function() {
+
+      escreveBoard("Se prepare!")
+      
+      const idInterval = setInterval(function(){
+      
+      if (bStartouGame){
+         
+         CriaLabirinto();
+         clearInterval(idInterval);
+         RemoveCaixasBloqueio();
+         MostraSetaFlag();
+      }  
+        attr_text--;
+        
+        if (attr_text == 0)
+        {
+           bStartouGame = true;
+           escreveBoard("CORRA");
+          
+           
+        }
+        else if (attr_text > 0)
+        {
+          SomCount(); 
+          escreveBoard(attr_text)
+        }      
+
+      }, 1000);
+
+
+    }, 2000); // Inicia o contador depois de 5 segundos
+
+}
+
+function RemoveMaze(){
+   removeByID("maze1");
+}
+
+function RemoveCaixasBloqueio(){
+   removeByID("caixas-bloqueio");
+}
+
+function removeByID(id)
+{
+   var cena = document.getElementById("labirinto");
+   var bloqueios = document.getElementById(id);
+   cena.removeChild(bloqueios);
+}
+
+function SomCount(){
+   var som = new Howl({
+      src: ['assets/sound/count.mp3']
+  });
+  som.play()
+}
+
+function SomWin(){
+   var som = new Howl({
+      src: ['assets/sound/win.mp3']
+  });
+  som.play()
+}
+
+function contadorMaze(){
    var seconds = 0;
-   var counter;
 
-   seconds = 0;
-      counter = setInterval(function(){
-        var board = document.getElementById('board');
-        var attr_text = board.getAttribute('value');
+   setTimeout(function() {
+      var board = document.getElementById('board');
+      board.setAttribute('value','');
+      board.setAttribute('position','0 2 -2.72518');
+      board.setAttribute('scale','3 3 3');
 
+
+      const idInterval = setInterval(function(){
+         var attr_text = board.getAttribute('value');
+         
+         if(bTimeoutMAze || bSaiuMaze){
+            clearInterval(idInterval);
+            RemoveMaze();
+            tempoEsgotado();
+         } 
 
         attr_text = seconds++;
-        board.setAttribute('value',attr_text);
-      },1000);
-    
+        
+        if (attr_text == 10 && !bSaiuMaze)
+        {
+           bTimeoutMAze = true;
+           return;
+        }
+        else if (attr_text < 10 && !bSaiuMaze)
+          escreveBoard(attr_text);
 
+      }, 1000);
+    }, 8000); // Inicia o contador depois de 5 segundos
 
-   pilar.addEventListener('collide', function (event) {
-      if (!bCriouMaze){
-         const cena = document.querySelector('#labirinto');
-         const novaMaze = document.createElement('a-entity');
-         novaMaze.setAttribute('id', 'maze1');
-         novaMaze.setAttribute('maze', 'size: 10 10; wall: #wall-one; cap: #end-cap 0.4; open: E 0 2 7 5 ;');
-         novaMaze.setAttribute('position', '-10 0 0');
-         novaMaze.setAttribute('rotation', '0 0 0');    
+}
 
-         cena.appendChild(novaMaze);
+function escreveBoard(texto){
+   var board = document.getElementById('board');
+   board.setAttribute('value',texto)
+}
 
-         bCriouMaze = true;
+function escreveBoardLetraMenor(texto){
+   var board = document.getElementById('board');
+   board.setAttribute('value',texto)
+   board.setAttribute('scale',"1.5 1.5 1.5")
+}
+
+function tempoEsgotado(){
+   if(bTimeoutMAze)
+   {
+      escreveBoard("TEMPO EGOSTADO")
+
+      setTimeout(function() {
+         escreveBoardLetraMenor("Encoste na Bandeira");
+         visibleObj('compass-arrow','false');
+         visibleObj('plataforma','false');
+      },5000);
+   }
+}
+
+function RegistraPuloTeclaEspaco(){
+   document.addEventListener("keydown", function(event) {
+      if (event.keyCode == 32) { // barra de espaço
+        var playerEl = document.querySelector("#player");
+        playerEl.setAttribute("velocity", {x: 0, y: 15, z: 0});
       }
+    });
+}
+
+function visibleObj(id,attr){
+   var obj = document.getElementById(id);
+   obj.setAttribute('visible',attr)
+}
+
+function MensagemComTimeout(str1,str2,timeout){
+   
+   escreveBoardLetraMenor(str1);
+
+   setTimeout(function() {
+      escreveBoardLetraMenor(str2);
+   },timeout);
+}
+
+function MensagemChegada(bTimeoutMAze)
+{
+  if (bTimeoutMAze)
+    escreveBoardLetraMenor("Suba na escada!");
+  else
+    MensagemComTimeout("Parabens","Suba na escada!",5000);
+
+}
+
+function EventoChegada(){
+   
+   const chegadaEl = document.getElementById('flag');
+
+   // Adiciona um ouvinte de eventos de colisão à entidade do jogador
+   chegadaEl.addEventListener('collide', function(e) {
+     
+      // Verifica se a entidade colidida é a imagem de chegada
+      MensagemChegada(bTimeoutMAze);
+
+     visibleObj('compass-arrow','false');
+     visibleObj('plataforma','false');
+
+     SomWin();
+     removeByID('flag');
+     
+     bSaiuMaze = true;
+
+     setTimeout(function() {
+      
+      visibleObj('plataforma-final','true');
+      visibleObj('escada','true');
+      visibleObj('caixa-criativa','true');
+      visibleObj('esfera','true');
+      visibleObj('cone','true');
+      ObjetoFinaisCaindo();
+
+     },2500);
+   
    });
+   
+}
+
+function ObjetoFinaisCaindo(){
+  var caixa =  document.querySelector('#caixa-criativa');
+  var cone =  document.querySelector('#cone');
+  var esfera =  document.querySelector('#esfera');
+  
+  caixa.setAttribute('dynamic-body','');
+  cone.setAttribute('dynamic-body','');
+  esfera.setAttribute('dynamic-body','');
+}
+
+function MostraSetaFlag(){
+   visibleObj('compass-arrow','true');
+   visibleObj('flag','true');
+}
+
+function eventoCliqueCaixaFinal(){
+   
+   var CaixaFinal =  document.getElementById("caixa-final");
+
+   btnIniciar.addEventListener('click', function() {
+
+   });
+
+}
+
+function CriaLabirinto(){
+   const cena = document.querySelector('#labirinto');
+   const novaMaze = document.createElement('a-entity');
+   novaMaze.setAttribute('id', 'maze1');
+   novaMaze.setAttribute('maze', 'size: 10 10; wall: #wall-one; cap: #end-cap 0.4; open: E 0 2 7 5 ;');
+   novaMaze.setAttribute('position', '-10 0 0');
+   novaMaze.setAttribute('rotation', '0 0 0');    
+   cena.appendChild(novaMaze);
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+   EventoChegada();
+   RegistraPuloTeclaEspaco();
+   StartGame();
 });
--- scripts/principal.js
+++ scripts/principal.js
@@ -1,33 +1,59 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  AFRAME.registerComponent("click-drag", {
+    schema: {
+      enabled: { default: true },
+    },
 
-// Seleciona as entidades das pirâmides
-var piramide1 = document.querySelector('#piramide1');
-var piramide2 = document.querySelector('#piramide2');
+    init: function () {
+      this.mouseDown = false;
+      this.originalPos = new THREE.Vector3();
+      this.newPos = new THREE.Vector3();
+      this.directionVec = new THREE.Vector3();
+      this.el.sceneEl.addEventListener("mousedown", this.onMouseDown.bind(this));
+      this.el.sceneEl.addEventListener("mouseup", this.onMouseUp.bind(this));
+      this.el.sceneEl.addEventListener("mousemove", this.onMouseMove.bind(this));
+    },
 
-var piramide3 = document.querySelector('#piramide3');
-var piramide4 = document.querySelector('#piramide4');
+    onMouseDown: function (e) {
+      if (!this.data.enabled) return;
+      const intersection = this.getIntersection(e);
+      if (intersection && intersection.object.el && intersection.object.el === this.el) {
+        this.mouseDown = true;
+        this.originalPos.copy(this.el.object3D.position);
+      }
+    },
 
-console.log("teste")
-// Alterna a cor das pirâmides a cada 1 segundo
-setInterval(function() {
-  // Alterna a cor da pirâmide 1
-  if (piramide1.getAttribute('material').color == 'red') {
-    piramide1.setAttribute('material', 'color', '#ffcc00');
-    piramide2.setAttribute('material', 'color', '#ffcc00');
-    
-  } else {
-    piramide1.setAttribute('material', 'color', 'red');
-    piramide2.setAttribute('material', 'color', 'red');
-  }
+    onMouseUp: function () {
+      this.mouseDown = false;
+    },
 
-  // Alterna a cor da pirâmide 2
-  if (piramide3.getAttribute('material').color == 'blue') {
-    piramide3.setAttribute('material', 'color', '#ffcc00');
-    piramide4.setAttribute('material', 'color', '#ffcc00');
-  } else {
-    piramide3.setAttribute('material', 'color', 'blue');
-    piramide4.setAttribute('material', 'color', 'blue');
-  }
-}, 1000);
+    onMouseMove: function (e) {
+      if (!this.mouseDown) return;
+      const intersection = this.getIntersection(e);
+      if (intersection) {
+        this.newPos.copy(intersection.point);
+        
+        // Projetar a posição atual do objeto na superfície do plano
+        const planeNormal = new THREE.Vector3(0, 1, 0);
+        const projectedPoint = this.newPos.projectOnPlane(planeNormal);
+        
+        this.directionVec.copy(projectedPoint).sub(this.originalPos);
+        
+        // Verificar se o objeto se move apenas ao longo do plano
+        if (Math.abs(this.directionVec.y) < 0.001) {
+          this.el.object3D.position.add(this.directionVec);
+        }
+      }
+    },
 
+    getIntersection: function (e) {
+      const mouse = new THREE.Vector2((e.clientX / window.innerWidth) * 2 - 1, -(e.clientY / window.innerHeight) * 2 + 1);
+      const raycaster = new THREE.Raycaster();
+      raycaster.setFromCamera(mouse, this.el.sceneEl.camera);
+      const intersections = raycaster.intersectObjects(this.el.sceneEl.object3D.children, true);
+      return intersections[0] ? intersections[0] : null;
+    },
+  });
+
+  // Restante do código
 });
